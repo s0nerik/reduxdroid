dff4386cfb4af3bc97ebd255155e427e32a85a97
Alex Isaienko <thesonerik@gmail.com>
2018-10-03 11:13:26 +0300

    Removed stuff not related to redux implementation.

--- core/src/main/java/com/github/s0nerik/reduxdroid/core/databinding/Adapters.kt
+++
@@ -1,7 +0,0 @@
-package com.github.s0nerik.reduxdroid.core.databinding
-
-import android.view.View
-import androidx.databinding.BindingConversion
-
-@BindingConversion
-fun convertBooleanToVisibility(isVisible: Boolean) = if (isVisible) View.VISIBLE else View.GONE
