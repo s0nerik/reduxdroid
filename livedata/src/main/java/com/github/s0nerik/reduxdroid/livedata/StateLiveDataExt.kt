f9a485becade0a8c773fd89298cc4b6e84900b44
Alex Isaienko <thesonerik@gmail.com>
2018-10-04 15:50:27 +0300

    StateLiveDataExt: replaced noinline with crossinline for all property selectors

--- livedata/src/main/java/com/github/s0nerik/reduxdroid/livedata/StateLiveDataExt.kt
+++ livedata/src/main/java/com/github/s0nerik/reduxdroid/livedata/StateLiveDataExt.kt
@@ -5,16 +5,16 @@ import com.github.s0nerik.reduxdroid.core.state.AppState
 import com.shopify.livedataktx.map
 import com.shopify.livedataktx.nonNull
 
-inline fun <reified S, T : Any> LiveData<AppState>.get(noinline mapper: (S) -> T) =
+inline fun <reified S, T : Any> LiveData<AppState>.get(crossinline mapper: (S) -> T) =
         nonNull().map { state -> mapper(state.get()) }
 
-inline fun <reified S, T : Any> LiveData<AppState>.getNonNull(noinline mapper: (S) -> T?) =
+inline fun <reified S, T : Any> LiveData<AppState>.getNonNull(crossinline mapper: (S) -> T?) =
         nonNull().map { state -> mapper(state.get()) }
                 .nonNull()
                 .map { it!! }
 
-inline fun <reified S, T : Any> LiveData<AppState>.getNullable(noinline mapper: (S) -> T?) =
+inline fun <reified S, T : Any> LiveData<AppState>.getNullable(crossinline mapper: (S) -> T?) =
         nonNull().map { state -> mapper(state.get()) }
 
-inline fun <reified S, T : Any> LiveData<AppState>.getAsString(noinline mapper: (S) -> T?, nullStr: String = "") =
+inline fun <reified S, T : Any> LiveData<AppState>.getAsString(crossinline mapper: (S) -> T?, nullStr: String = "") =
         nonNull().map { state -> mapper(state.get())?.toString() ?: nullStr }
