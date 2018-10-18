6511f9337fc23d66d0e4b905a12c403d3f21982e
Alex Isaienko <thesonerik@gmail.com>
2018-10-18 19:10:55 +0300

    StateLiveDataExt: improved Kotlin type inference

--- livedata/src/main/java/com/github/s0nerik/reduxdroid/livedata/StateLiveDataExt.kt
+++ livedata/src/main/java/com/github/s0nerik/reduxdroid/livedata/StateLiveDataExt.kt
@@ -7,16 +7,16 @@ import com.github.s0nerik.reduxdroid.core.state.AppState
 
 @MainThread
 inline fun <reified S> LiveData<AppState>.get() =
-        Transformations.map(this) { it.get<S>() }
+        Transformations.map(this) { it.get<S>() } as LiveData<S>
 
 @MainThread
 inline fun <reified S, T : Any> LiveData<AppState>.get(crossinline selector: (S) -> T) =
-        Transformations.map(this) { it.get(selector) }
+        Transformations.map(this) { it.get(selector) } as LiveData<T>
 
 @MainThread
 inline fun <reified S, T : Any> LiveData<AppState>.get(crossinline selector: (S) -> T?, default: T) =
-        Transformations.map(this) { it.get(selector) ?: default }
+        Transformations.map(this) { it.get(selector) ?: default } as LiveData<T>
 
 @MainThread
 inline fun <reified S, T : Any?> LiveData<AppState>.getNullable(crossinline selector: (S) -> T) =
-        Transformations.map(this) { it.get(selector) }
+        Transformations.map(this) { it.get(selector) } as LiveData<T>
