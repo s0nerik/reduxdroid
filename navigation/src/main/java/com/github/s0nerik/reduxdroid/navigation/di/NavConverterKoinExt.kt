8f9a31051f32427a6bcb832b9cde9375eaa5877b
Alex Isaienko <thesonerik@gmail.com>
2018-12-07 11:56:24 +0200

    Navigation converter extensions

---
+++ navigation/src/main/java/com/github/s0nerik/reduxdroid/navigation/di/NavConverterKoinExt.kt
@@ -0,0 +1,30 @@
+package com.github.s0nerik.reduxdroid.navigation.di
+
+import androidx.annotation.IdRes
+import com.github.s0nerik.reduxdroid.core.di.actionConverter
+import com.github.s0nerik.reduxdroid.navigation.DidNavigate
+import org.koin.dsl.context.ModuleDefinition
+
+fun ModuleDefinition.convertNavForwardFrom(@IdRes id: Int, actionProvider: () -> Any) =
+        actionConverter<DidNavigate.Forward>(
+                filter = { it.from == id },
+                converter = { actionProvider() }
+        )
+
+fun ModuleDefinition.convertNavForwardTo(@IdRes id: Int, actionProvider: () -> Any) =
+        actionConverter<DidNavigate.Forward>(
+                filter = { it.to == id },
+                converter = { actionProvider() }
+        )
+
+fun ModuleDefinition.convertNavBackFrom(@IdRes id: Int, actionProvider: () -> Any) =
+        actionConverter<DidNavigate.Back>(
+                filter = { it.from == id },
+                converter = { actionProvider() }
+        )
+
+fun ModuleDefinition.convertNavBackTo(@IdRes id: Int, actionProvider: () -> Any) =
+        actionConverter<DidNavigate.Back>(
+                filter = { it.to == id },
+                converter = { actionProvider() }
+        )
