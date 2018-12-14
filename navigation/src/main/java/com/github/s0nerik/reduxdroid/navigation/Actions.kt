fb5ae93d7f708dc941b023d5b206137457030fe0
Alex Isaienko <thesonerik@gmail.com>
2018-12-14 15:45:03 +0200

    Temporarily made DidNavigate.Back and DidNavigate.Forward action constructors public.

--- navigation/src/main/java/com/github/s0nerik/reduxdroid/navigation/Actions.kt
+++ navigation/src/main/java/com/github/s0nerik/reduxdroid/navigation/Actions.kt
@@ -11,12 +11,12 @@ sealed class DidNavigate(
         @IdRes open val from: Int,
         @IdRes open val to: Int
 ) {
-    data class Forward internal constructor(
+    data class Forward(
             @IdRes override val from: Int,
             @IdRes override val to: Int
     ) : DidNavigate(from, to)
 
-    data class Back internal constructor(
+    data class Back(
             @IdRes override val from: Int,
             @IdRes override val to: Int
     ) : DidNavigate(from, to)
