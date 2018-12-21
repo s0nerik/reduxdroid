d8b5993a547d4b15e6914318aa181e2ebb64813e
Alex Isaienko <thesonerik@gmail.com>
2018-12-21 17:40:28 +0200

    Added some documentation for Nav.Forward and Nav.Back

--- navigation/src/main/java/com/github/s0nerik/reduxdroid/navigation/Actions.kt
+++ navigation/src/main/java/com/github/s0nerik/reduxdroid/navigation/Actions.kt
@@ -3,11 +3,22 @@ package com.github.s0nerik.reduxdroid.navigation
 import androidx.annotation.IdRes
 
 sealed class Nav {
+    /**
+     * Dispatching this action will result in navigation to a specified destination.
+     *
+     * **CAUTION**: This action is intended to be instantiated inside an `actionConverter` definition.
+     */
     data class Forward(@IdRes val to: Int) : Nav()
-    data class Back(@IdRes val to: Int? = null, val inclusive: Boolean = false) : Nav()
+
+    /**
+     * Dispatching this action will result in back navigation (as if the "Back" button was pressed).
+     *
+     * *CAUTION*: This action is intended to be instantiated inside an `actionConverter` definition.
+     */
+    data class Back(@IdRes val to: Int?, val inclusive: Boolean) : Nav()
 }
 
-data class DidNavigate(
+data class DidNavigate internal constructor(
         @IdRes val graph: Int,
         @IdRes val from: Int,
         @IdRes val to: Int,
