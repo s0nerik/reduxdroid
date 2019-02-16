e927a78d195f8903ce3d349aac2ac61bc1ed9dee
Alex Isaienko <thesonerik@gmail.com>
2019-02-16 03:19:20 +0200

    Test adjustments

--- core/src/test/java/com/github/s0nerik/reduxdroid/core/ActionConverterMiddlewareTest.kt
+++ core/src/test/java/com/github/s0nerik/reduxdroid/core/ActionConverterMiddlewareTest.kt
@@ -1,18 +1,16 @@
 package com.github.s0nerik.reduxdroid.core
 
 import com.github.s0nerik.reduxdroid.core.di.actionConverter
-import com.github.s0nerik.reduxdroid.core.di.middlewares
-import com.github.s0nerik.reduxdroid.core.middleware.middleware
-import com.github.s0nerik.reduxdroid.core.state.AppState
+import com.github.s0nerik.reduxdroid.testing.TestMiddleware
+import com.github.s0nerik.reduxdroid.testing.testModules
 import com.github.s0nerik.reduxdroid.util.ReduxConfig
-import me.tatarka.redux.middleware.TestMiddleware
 import org.junit.After
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
-import org.koin.dsl.ModuleDeclaration
 import org.koin.dsl.module
 import org.koin.test.KoinTest
 import org.koin.test.inject
@@ -29,29 +27,19 @@ class ActionConverterMiddlewareTest : KoinTest {
         object A2 : A()
     }
 
-    private val store: StateStore by inject()
     private val dispatcher: ActionDispatcher by inject()
+    private val testMiddleware: TestMiddleware by inject()
 
-    private fun startTestKoin(testModuleDefinition: ModuleDeclaration): TestMiddleware<AppState, Any, Any> {
-        lateinit var testMiddleware: TestMiddleware<AppState, Any, Any>
+    private val redCfg = ReduxConfig.configs
 
+    @Before
+    fun setup() {
         startKoin {
             modules(
                     Module().module,
-                    module(moduleDeclaration = testModuleDefinition)
+                    *testModules().toTypedArray()
             )
         }
-
-        loadKoinModules(module {
-            testMiddleware = TestMiddleware<AppState, Any, Any>(store.store)
-            middlewares {
-                listOf(
-                        middleware<Any, Any> { next, action -> testMiddleware.dispatch(next, action) }
-                )
-            }
-        })
-
-        return testMiddleware
     }
 
     @After
@@ -62,82 +50,94 @@ class ActionConverterMiddlewareTest : KoinTest {
 
     @Test
     fun `actionConverter doesn't handle inheritance`() {
-        val testMiddleware = startTestKoin {
-            actionConverter<A> { _, _, dispatch -> dispatch(Action1) }
-            actionConverter<A.A1> { _, _, dispatch -> dispatch(Action2) }
-        }
+        loadKoinModules(
+                module {
+                    actionConverter<A> { _, _, dispatch -> dispatch(Action1) }
+                    actionConverter<A.A1> { _, _, dispatch -> dispatch(Action2) }
+                }
+        )
 
         dispatcher.dispatch(A.A1)
 
-        Assert.assertArrayEquals(arrayOf(A.A1, Action2), testMiddleware.actions().toTypedArray())
+        Assert.assertArrayEquals(arrayOf(A.A1, Action2), testMiddleware.actions.toTypedArray())
     }
 
     @Test
     fun `test simple case`() {
-        val testMiddleware = startTestKoin {
-            actionConverter<Action1> { _, _, dispatch -> dispatch(Action2) }
-        }
+        loadKoinModules(
+                module {
+                    actionConverter<Action1> { _, _, dispatch -> dispatch(Action2) }
+                }
+        )
 
         dispatcher.dispatch(Action1)
 
-        Assert.assertEquals(2, testMiddleware.actions().size)
+        Assert.assertEquals(2, testMiddleware.actions.size)
     }
 
     @Test
     fun `multiple actionConverters don't produce original action multiple times`() {
-        val testMiddleware = startTestKoin {
-            actionConverter<Action1> { _, _, dispatch -> dispatch(Action2) }
-            actionConverter<Action1> { _, _, dispatch -> dispatch(Action3) }
-            actionConverter<Action1> { _, _, dispatch -> dispatch(Action4) }
-        }
+        loadKoinModules(
+                module {
+                    actionConverter<Action1> { _, _, dispatch -> dispatch(Action2) }
+                    actionConverter<Action1> { _, _, dispatch -> dispatch(Action3) }
+                    actionConverter<Action1> { _, _, dispatch -> dispatch(Action4) }
+                }
+        )
 
         dispatcher.dispatch(Action1)
 
-        Assert.assertArrayEquals(arrayOf(Action1, Action2, Action3, Action4), testMiddleware.actions().toTypedArray())
+        Assert.assertArrayEquals(arrayOf(Action1, Action2, Action3, Action4), testMiddleware.actions.toTypedArray())
     }
 
     @Test
     fun `actionConverters can return actions that itself will be converted to another actions`() {
-        val testMiddleware = startTestKoin {
-            actionConverter<Action1> { _, _, dispatch -> dispatch(Action2) }
-            actionConverter<Action2> { _, _, dispatch -> dispatch(Action3) }
-            actionConverter<Action3> { _, _, dispatch -> dispatch(Action4) }
-        }
+        loadKoinModules(
+                module {
+                    actionConverter<Action1> { _, _, dispatch -> dispatch(Action2) }
+                    actionConverter<Action2> { _, _, dispatch -> dispatch(Action3) }
+                    actionConverter<Action3> { _, _, dispatch -> dispatch(Action4) }
+                }
+        )
 
         dispatcher.dispatch(Action1)
 
-        Assert.assertArrayEquals(arrayOf(Action1, Action2, Action3, Action4), testMiddleware.actions().toTypedArray())
+        Assert.assertArrayEquals(arrayOf(Action1, Action2, Action3, Action4), testMiddleware.actions.toTypedArray())
     }
 
     @Test
     fun `actionConverter dispatches converted actions in registration order`() {
-        val testMiddleware = startTestKoin {
-            actionConverter<Action1> { _, _, dispatch -> dispatch(Action2) }
-            actionConverter<Action2> { _, _, dispatch -> dispatch(Action3) }
-            actionConverter<Action3> { _, _, dispatch -> dispatch(Action4) }
-            actionConverter<Action1> { _, _, dispatch -> dispatch(Action2) }
-        }
+        loadKoinModules(
+                module {
+                    actionConverter<Action1> { _, _, dispatch -> dispatch(Action2) }
+                    actionConverter<Action2> { _, _, dispatch -> dispatch(Action3) }
+                    actionConverter<Action3> { _, _, dispatch -> dispatch(Action4) }
+                    actionConverter<Action1> { _, _, dispatch -> dispatch(Action2) }
+                }
+        )
 
         dispatcher.dispatch(Action1)
 
-        Assert.assertArrayEquals(arrayOf(Action1, Action2, Action3, Action4, Action2, Action3, Action4), testMiddleware.actions().toTypedArray())
+        Assert.assertArrayEquals(arrayOf(Action1, Action2, Action3, Action4, Action2, Action3, Action4), testMiddleware.actions.toTypedArray())
     }
 
     @Test
     fun `actionConverter can dispatch multiple actions, each actionConverter maintains dispatch orders`() {
-        val testMiddleware = startTestKoin {
-            actionConverter<Action1> { _, _, dispatch ->
-                dispatch(Action2)
-                dispatch(Action3)
-            }
-            actionConverter<Action2> { _, _, dispatch ->
-                dispatch(Action4)
-                dispatch(Action5)
-            }
-        }
+        loadKoinModules(
+                module {
+                    actionConverter<Action1> { _, _, dispatch ->
+                        dispatch(Action2)
+                        dispatch(Action3)
+                    }
+                    actionConverter<Action2> { _, _, dispatch ->
+                        dispatch(Action4)
+                        dispatch(Action5)
+                    }
+                }
+        )
 
         dispatcher.dispatch(Action1)
 
-        Assert.assertArrayEquals(arrayOf(Action1, Action2, Action4, Action5, Action3), testMiddleware.actions().toTypedArray())
+        Assert.assertArrayEquals(arrayOf(Action1, Action2, Action4, Action5, Action3), testMiddleware.actions.toTypedArray())
     }
 }
