0d04debab90d25f9081609084f36936c49331349
Alex Isaienko <thesonerik@gmail.com>
2019-02-17 18:09:33 +0200

    Lazy initial state provider

--- core/src/main/java/com/github/s0nerik/reduxdroid/core/di/InitialStatesKoinExt.kt
+++ core/src/main/java/com/github/s0nerik/reduxdroid/core/di/InitialStatesKoinExt.kt
@@ -1,16 +1,17 @@
 package com.github.s0nerik.reduxdroid.core.di
 
 import com.github.s0nerik.reduxdroid.util.ReduxConfig
+import org.koin.core.definition.DefinitionContext
 import org.koin.core.module.Module
 import kotlin.reflect.KClass
 
 @PublishedApi
 internal const val INITIAL_STATES = "INITIAL_STATES"
 
-internal val initialStates
-    get() = ReduxConfig.getUniqueKeyMap<KClass<*>, Any>(INITIAL_STATES)
+internal val initialStateProviders
+    get() = ReduxConfig.getUniqueKeyMap<KClass<*>, DefinitionContext.() -> Any>(INITIAL_STATES)
 
-inline fun <reified S : Any> Module.state(state: S) = ReduxConfig.addUniqueKeyMapEntry(
+inline fun <reified S : Any> Module.state(noinline state: DefinitionContext.() -> S) = ReduxConfig.addUniqueKeyMapEntry(
         configKey = INITIAL_STATES,
         key = S::class,
         value = state,
