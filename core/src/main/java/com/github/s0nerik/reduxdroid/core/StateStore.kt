6fc4acefb8077a6dcca388cec69183f633c67594
Alex Isaienko <thesonerik@gmail.com>
2019-03-22 18:10:18 +0200

    StateStore: fixed listener leaks due to Kotlin-Java interop.

--- core/src/main/java/com/github/s0nerik/reduxdroid/core/StateStore.kt
+++ core/src/main/java/com/github/s0nerik/reduxdroid/core/StateStore.kt
@@ -6,6 +6,8 @@ import me.tatarka.redux.SimpleStore
 class StateStore(initialState: AppState) {
     internal val store = SimpleStore<AppState>(initialState)
 
+    private val realListeners = mutableMapOf<(AppState) -> Unit, StoreListener>()
+
     var state: AppState
         get() = store.state
         set(newState) {
@@ -13,10 +15,24 @@ class StateStore(initialState: AppState) {
         }
 
     fun addListener(listener: (AppState) -> Unit) {
-        store.addListener(listener)
+        if (realListeners[listener] != null)
+            return
+
+        val wrappedListener = StoreListener(listener)
+        store.addListener(wrappedListener)
     }
 
     fun removeListener(listener: (AppState) -> Unit) {
-        store.removeListener(listener)
+        realListeners[listener]?.let {
+            store.removeListener(it)
+        }
+    }
+}
+
+private class StoreListener(
+        private val listener: (AppState) -> Unit
+) : SimpleStore.Listener<AppState> {
+    override fun onNewState(state: AppState) {
+        listener(state)
     }
 }
